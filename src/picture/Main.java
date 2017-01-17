0fe2da69d8785204131ba1cdd70fe5038c1a5928
cristina <as9716@ic.ac.uk>
2017-01-17 16:02:11 +0000

    hw

--- src/picture/Main.java
+++ src/picture/Main.java
@@ -3,7 +3,56 @@ package picture;
 public class Main {
 
   public static void main(String[] args) {
-    // TODO: Implement this.
-    System.err.println("TODO: Implement main");
+    Picture initialPicture = getPicture(args);
+    Picture modifiedPicture = applyProcess(initialPicture, args);
+    savePicture(modifiedPicture, args);
+
   }
+
+  private static Picture getPicture(String[] args) {
+    String location = args[args.length - 2];
+    return Utils.loadPicture(location);
+  }
+
+  private static void savePicture(Picture modifiedPicture, String[] args) {
+    String destination = args[args.length - 1];
+    Utils.savePicture(modifiedPicture, destination);
+  }
+
+  private static Picture applyProcess(Picture picture, String[] args) {
+    String detail, process = args[0];
+    Process processPicture = new Process(picture);
+    switch (process) {
+      case "invert": processPicture.invert(); break;
+      case "grayscale": processPicture.grayscale(); break;
+      case "rotate": {
+        detail = args[1];
+        switch (detail) {
+          case "90": processPicture.rotate90(); break;
+          case "180": processPicture.rotate180(); break;
+          case "270": processPicture.rotate270(); break;
+        }
+      } break;
+      case "flip": {
+        detail = args[1];
+        switch (detail) {
+          case "H": processPicture.flipHorizontal(); break;
+          case "V": processPicture.flipVertical(); break;
+        }
+      } break;
+      case "blend": {
+        int numberOfPictures = args.length - 2;
+        Picture[] pictures = new Picture[numberOfPictures];
+        for(int i = 0; i < numberOfPictures; i++) {
+          pictures[i] = Utils.loadPicture(args[i + 1]);
+        }
+        processPicture.blend(pictures);
+      } break;
+      case "blur": processPicture.blur(); break;
+    }
+
+    return processPicture.getPicture();
+  }
+
+
 }
---
+++ src/picture/Process.java
@@ -0,0 +1,151 @@
+package picture;
+
+public class Process {
+
+    private Picture picture;
+    private int width, height;
+
+    public Process(Picture picture) {
+        this.picture = picture;
+        this.height = picture.getHeight();
+        this.width = picture.getWidth();
+    }
+
+    public Picture getPicture() {
+        return this.picture;
+    }
+
+    public void invert() {
+
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                int red = 255 - pixel.getRed();
+                int blue = 255 - pixel.getBlue();
+                int green = 255 - pixel.getGreen();
+                Color color = new Color(red, green, blue);
+                picture.setPixel(i, j, color);
+
+            }
+        }
+
+
+    }
+
+    public void grayscale() {
+
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                int avg = (pixel.getRed() + pixel.getBlue() + pixel.getGreen()) / 3;
+                Color color = new Color(avg, avg, avg);
+                picture.setPixel(i, j, color);
+
+            }
+        }
+
+    }
+
+    public void rotate90() {
+        Picture newPicture = Utils.createPicture(height, width);
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                newPicture.setPixel(j, i, pixel);
+            }
+        }
+        picture = newPicture;
+    }
+
+    public void rotate180() {
+        Picture newPicture = Utils.createPicture(width, height);
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                newPicture.setPixel(width - 1 - i, height -1 - j, pixel);
+            }
+        }
+        picture = newPicture;
+    }
+
+    public void rotate270() {
+        rotate180();
+        rotate90();
+    }
+
+    public void flipHorizontal() {
+        Picture newPicture = Utils.createPicture(width, height);
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                newPicture.setPixel(width - 1 - i, j, pixel);
+            }
+        }
+        picture = newPicture;
+    }
+
+    public void flipVertical() {
+        Picture newPicture = Utils.createPicture(width, height);
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                Color pixel = picture.getPixel(i, j);
+                newPicture.setPixel(i, height - 1 - j, pixel);
+            }
+        }
+        picture = newPicture;
+    }
+
+    public void blend(Picture[] pictures) {
+        int minWidth = width;
+        int minHeight = height;
+        Picture newPicture = Utils.createPicture(minWidth, minHeight);
+        int numberOfPictures = pictures.length;
+        for(int i = 0; i < numberOfPictures; i++) {
+            minWidth = Math.min(minWidth, pictures[i].getWidth());
+            minHeight = Math.min(minHeight, pictures[i].getHeight());
+        }
+        for(int i = 0; i < minWidth; i++) {
+            for(int j = 0; j < minHeight; j++) {
+                Color pixel = pictures[0].getPixel(i, j);
+                for(int m = 1; m < numberOfPictures; m++) {
+                    addPixel(pixel, pictures[m].getPixel(i, j));
+                }
+                avgPixel(pixel, numberOfPictures);
+                newPicture.setPixel(i, j, pixel);
+            }
+        }
+        picture = newPicture;
+    }
+
+
+    public void blur() {
+        Picture newPicture = picture;
+        for(int i = 1; i < width - 1; i++) {
+            for(int j = 1; j < height - 1; j++) {
+                Color pixel1 = new Color(0, 0, 0);
+                for(int m = i - 1; m <= i + 1; m++) {
+                    for (int n = j - 1; n <= j + 1; n++) {
+                        Color pixel2 = picture.getPixel(m, n);
+                        addPixel(pixel1, pixel2);
+                    }
+                }
+                avgPixel(pixel1, 9);
+                newPicture.setPixel(i, j, pixel1);
+            }
+        }
+        picture = newPicture;
+    }
+
+    private void addPixel(Color pixel1, Color pixel2) {
+        pixel1.setRed(pixel1.getRed() + pixel2.getRed());
+        pixel1.setBlue(pixel1.getBlue() + pixel2.getBlue());
+        pixel1.setGreen(pixel1.getGreen() + pixel2.getGreen());
+    }
+
+    private void avgPixel(Color pixel, int n) {
+        pixel.setRed(pixel.getRed() / n);
+        pixel.setBlue(pixel.getBlue() / n);
+        pixel.setGreen(pixel.getGreen() / n);
+    }
+
+}
